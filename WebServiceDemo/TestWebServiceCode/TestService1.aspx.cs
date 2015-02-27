93eda7e7c22d2804409d5282b7a6cbfe5f02d550
Alim Ul Karim <devorg.bd@gmail.com>
2015-02-28 00:10:08 +0600

    init

---
+++ WebServiceDemo/TestWebServiceCode/TestService1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebServiceDemo.TestWebServiceCode {
+    public partial class TestService1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            ServiceReference1.ServiceSample1SoapClient client = new ServiceReference1.ServiceSample1SoapClient();
+            var input = this.TextBox1.Text;
+
+            var output = client.HelloWorld(input);
+            this.Label1.Text = output;
+        }
+    }
+}
